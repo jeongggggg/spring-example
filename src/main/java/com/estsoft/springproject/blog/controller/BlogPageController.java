bf40c25264169bfc0a33ceeb5a7ea7a0ea831b3e
dejeong <ahjeong921@gmail.com>
2024-10-17 14:10:49 +0900

    feat : /new-article 뷰컨트롤러 생성        newArticle.html 페이지 생성

--- src/main/java/com/estsoft/springproject/blog/controller/BlogPageController.java
+++ src/main/java/com/estsoft/springproject/blog/controller/BlogPageController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -38,5 +39,19 @@ public class BlogPageController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article"; // article.html
+    }
+
+    // GET /new-articles?id=1
+    // GET /new-articles
+    @GetMapping("/new-article")
+    public String addArticle(@RequestParam(required = false) Long id, Model model){
+        if(id == null){ // 새로운 게시글 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else{ // 게시글 수정 : 기존 게시글 불러오기
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle"; // newArticle.html
     }
 }
