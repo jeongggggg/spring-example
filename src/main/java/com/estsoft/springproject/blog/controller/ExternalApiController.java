c2982d780afb7d88a33e1d82b59b8bc5d829f053
dejeong <ahjeong921@gmail.com>
2024-10-17 15:37:14 +0900

    feat : 외부 API 호출, restTemplate 사용하여 역직렬화

--- src/main/java/com/estsoft/springproject/blog/controller/ExternalApiController.java
+++ src/main/java/com/estsoft/springproject/blog/controller/ExternalApiController.java
@@ -1,10 +1,14 @@
 package com.estsoft.springproject.blog.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,11 +17,15 @@ public class ExternalApiController {
     public String callApi(){
         // 외부 API 호출
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> json = restTemplate.getForEntity(
-                "https://jsonplaceholder.typicode.com/posts", String.class);
 
-        log.info("StatusCode : {}", json.getStatusCode());
-        log.info(json.getBody());
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        // 외부 API 호출, 역직렬화 (restTemplate)
+        ResponseEntity<List<Content>> resultList =
+                restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>(){});
+        log.info("code : {}", resultList.getStatusCode());
+        log.info("{}", resultList.getBody());
+
 
         return "OK";
     }
