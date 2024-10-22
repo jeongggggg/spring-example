e535517e69a134090656ea3a08f8ea6b6686b5a5
dejeong <ahjeong921@gmail.com>
2024-10-22 15:14:51 +0900

    feat: 양방향 연관 관계 매핑

--- src/main/java/com/estsoft/springproject/domain/Team.java
+++ src/main/java/com/estsoft/springproject/domain/Team.java
@@ -1,9 +1,9 @@
 package com.estsoft.springproject.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.estsoft.springproject.entity.Member;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,4 +14,9 @@ public class Team {
 
     @Column
     private String name;
+
+    // 양방향 연관 관계 -> @OneToMany (X), 잘 사용하지 않음 성능에 좋지 않음, jpa 특성 상 팀 하나당 가지고 있는 여러 개의 묶음 들을 모두 조회 되기 때문에.
+    // 연관 관계의 주인 명시
+    @OneToMany(mappedBy = "tesm") // team 객체가 연관관계의 주인.
+    List<Member> members;
 }
